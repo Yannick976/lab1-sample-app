37bf09631327f659d0fccebc53c9ca29c7f3f2bd
yannick-prat <yannick.prat@edu.esiee.fr>
2025-11-26 21:52:08 +0100

    Update server to listen on all interfaces

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!\n');
 });
+
 const port = process.env.PORT || 8080;
-server.listen(port,() => {
-  console.log(`Listening on port ${port}`);
+const host = '0.0.0.0'; // écoute sur toutes les interfaces
+
+server.listen(port, host, () => {
+  console.log(`Listening on ${host}:${port}`);
 });
